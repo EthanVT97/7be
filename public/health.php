ae319b4e0e15e853f9a712a97174d6d84fefa764
EthanVT97 <gyi798107@gmail.com>
2024-12-26 00:22:53 +0630

    Simplify configuration for health check

--- public/health.php
+++ public/health.php
@@ -1,12 +1,7 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, OPTIONS');
 
-$health = [
-    'status' => 'healthy',
-    'timestamp' => time()
-];
-
-http_response_code(200);
-echo json_encode($health);
+echo json_encode([
+    'status' => 'healthy'
+]);
