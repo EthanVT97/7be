448d54e5470ea84df8d95469280470812435d358
EthanVT97 <gyi798107@gmail.com>
2024-12-26 00:30:48 +0630

    Fix deployment configuration and add health check endpoint

--- public/health.php
+++ public/health.php
@@ -3,5 +3,6 @@ header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
 echo json_encode([
-    'status' => 'healthy'
+    'status' => 'healthy',
+    'timestamp' => time()
 ]);
---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', 'php-error.log');
+
+// Rest of your application code 
